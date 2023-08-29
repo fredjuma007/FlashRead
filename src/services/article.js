392be43fc240b8262a0e296e588123d55b755afe
fred <fredjuma8@gmail.com>
2023-08-29 04:43:13 +0300

    ITS ALIVE!!!

--- src/services/article.js
+++ src/services/article.js
@@ -17,7 +17,7 @@ export const articleApi = createApi({
      }),
     endpoints: (builder) => ({
         getSummary: builder.query({
-            query: (params) => `summarize?url=${encodeURIComponent(params.articleUrl)}&length=3`,
+            query: (params) => `summarize?url=${encodeURIComponent(params.articleUrl)}&length=7`,
         }) // incase the articles has special characters we use encodeURIComponent
     }),
 });
