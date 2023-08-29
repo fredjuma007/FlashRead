c04df99962642e5fe1ad1f80358d00739bf0e404
fred <fredjuma8@gmail.com>
2023-08-29 03:34:19 +0300

    clear console errors

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,17 +1,28 @@
 import React, { useState, useEffect } from "react";
 import { copy, linkIcon, loader, tick } from "../assets";
 
+import { useLazyGetSummaryQuery } from "../services/article";
+
 const Demo = () => {
   const [article, setArticle] = useState({
     url: "",
     summary: "",
   }); //to store the articles
+
+  const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery(); //to get the articles from the api
   
   const handleSubmit = async (e) => {
-    alert("submitted")
+    e.preventDefault();
+
+    const { data } = await getSummary({ articleUrl: article.url });
+
+    if(data?.summary) {
+    const newArticle = { ...article, summary: data.summary };
+    setArticle(newArticle);
+    console.log(newArticle);
   }
+  };
  
-  
   return (
 <section className='mt-16 w-full max-w-xl'>
     {/* Search */}
--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import { Provider } from "react-redux";
 import App from './App.jsx'
 import { store } from './services/store';
 
+/*wrapping the app with a provider that contains the store with articles api*/
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
