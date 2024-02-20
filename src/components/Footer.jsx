7a168b5b4afc150f47e24a2b56a8798aa55013d1
fred <fredjuma8@gmail.com>
2024-02-20 19:27:17 +0300

    updated footer date

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="w-full bg-gray-900 text-white py-4 text-center">
-      <p>&copy; 2023 FlashRead</p>
+      <p>&copy; 2024 FlashRead</p>
     </footer>
   );
 };
